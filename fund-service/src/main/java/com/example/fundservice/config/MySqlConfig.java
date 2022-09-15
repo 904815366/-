3852ec62c3f99f0c3a5ba04254606fe955f9ed9f
blxglxx <1243392843@qq.com>
2022-09-15 19:22:28 +0800

    lx[0915]

---
+++ fund-service/src/main/java/com/example/fundservice/config/MySqlConfig.java
@@ -0,0 +1,9 @@
+package com.example.fundservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.fundservice.dao.mysql")
+public class MySqlConfig {
+}
---
+++ fund-service/src/main/java/com/example/fundservice/config/OpenFeignConfig.java
@@ -0,0 +1,9 @@
+package com.example.fundservice.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableFeignClients(basePackages = {"com.example.employee.api.client"})
+public class OpenFeignConfig {
+}
