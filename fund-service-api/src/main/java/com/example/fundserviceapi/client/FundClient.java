f04f9c1d71af69670a26ab8c387afa2cca682de8
blxglxx <1243392843@qq.com>
2022-09-20 09:20:17 +0800

    [9/20 19:53 lx]openFeign接收采购/出货消息(2.0)

--- fund-service-api/src/main/java/com/example/fundserviceapi/client/FundClient.java
+++ fund-service-api/src/main/java/com/example/fundserviceapi/client/FundClient.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient("fund-service")
 public interface FundClient {
-    //采购单消息
+    //采购单消息     出货单chdix/供应商gysid/结算账户accid/金额account
     @PostMapping("/billpay/addBillpayMsg/{cgdid}/{gysid}/{accid}/{account}")
     public ResponseResult<Void> getCgdMsg(@PathVariable("cgdid")Integer cgdid,
                                           @PathVariable("gysid")Integer gysid,
                                           @PathVariable("accid")Integer accid,
                                           @PathVariable("account")Double account);
-    //出货单消息
-    @PostMapping("/billpay/addBillreceMsg/{chdid}/{cstid}/{accid}/{accid}")
+    //出货单消息     采购单cgdix/客户cstid/结算账户accid/金额account
+    @PostMapping("/billpay/addBillreceMsg/{chdid}/{cstid}/{accid}/{account}")
     public ResponseResult<Void> getChdMsg(@PathVariable("chdid")Integer chdid,
                                           @PathVariable("cstid")Integer cstid,
                                           @PathVariable("accid")Integer accid,
