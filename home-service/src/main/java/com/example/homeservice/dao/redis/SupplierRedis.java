9da3550e03177c69bf702265f40e16a18ee33797
blxglxx <1243392843@qq.com>
2022-09-23 11:35:37 +0800

    删了home-service

--- home-service/src/main/java/com/example/homeservice/dao/redis/SupplierRedis.java
+++
@@ -1,9 +0,0 @@
-package com.example.homeservice.dao.redis;
-
-import com.example.homeservice.dao.redis.po.SupplierRedisPo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRedis extends CrudRepository<SupplierRedisPo,Long> {
-}
