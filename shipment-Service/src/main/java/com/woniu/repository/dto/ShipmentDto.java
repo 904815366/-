f056aa6b6ebbc3acbace587feabfabafb5a1ac2d
Tian <1149978754@qq.com>
2022-09-23 11:17:03 +0800

    xk[09-23 11:16]合并分支

--- shipment-Service/src/main/java/com/woniu/repository/dto/ShipmentDto.java
+++ shipment-Service/src/main/java/com/woniu/repository/dto/ShipmentDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 public class ShipmentDto {
@@ -20,7 +21,7 @@ public class ShipmentDto {
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime attime;
+    private Date attime;
 
     private String status;
 
